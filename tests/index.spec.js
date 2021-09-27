d7a0d8b64a04e025e08f26baf2138dd3b8637cad
Amnat Wongkhamkaeo <61160025@go.buu.ac.th>
2021-09-27 19:57:07 +0700

    HW2

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -10,6 +10,6 @@ describe('GET /', () => {
 
 describe('GET /users', () => {
     it('responds with "No user in the system"', (done) => {
-        request(app).get('/').expect('No user in the system', done)
+        request(app).get('/users').expect('No user in the system', done)
     })
-})
+}) 
